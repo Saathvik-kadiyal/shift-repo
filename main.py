12c3d6d4d642c2f0dd367cd90551d363e57d0776
Saathvik Kadiyal <saathvikkadiyal@gmail.com>
2026-02-17 16:07:17 +0530

    Modify CORS middleware to disallow credentials
    Updated CORS middleware settings to disallow credentials.

--- main.py
+++ main.py
@@ -13,15 +13,24 @@ from app import route
 app = FastAPI()
 Base.metadata.create_all(bind=engine)
 
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=[
+#         "*",
+#     ],
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "*",
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],   # allow all origins
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 
 app.include_router(route.router)
